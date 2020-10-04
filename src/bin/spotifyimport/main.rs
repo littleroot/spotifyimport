3da84fe3493962567738a8bc37d07783f8282421
Nishanth Shanmugham <nishanths@utexas.edu>
2020-10-04 21:50:20 +0530

    skeleton for main

--- src/bin/spotifyimport/main.rs
+++ src/bin/spotifyimport/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("hello, world");
+use anyhow::{anyhow, Error};
+use log::*;
+use logosaurus::{self, Logger, L_SHORT_FILE, L_STD};
+use std::process;
+
+#[tokio::main]
+async fn main() {
+    let logger = Logger::builder()
+        .set_prefix("spotifyimport: ")
+        .set_flags(L_STD | L_SHORT_FILE)
+        .build();
+    logosaurus::init(logger).unwrap();
+
+    if let Err(e) = run() {
+        error!("{}", e);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Error> {
+    Err(anyhow!("not implemented"))
 }
